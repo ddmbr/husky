78c78b243a5584ade68051481e246a4053be71e3
MiaoLoud <liumiaoo@126.com>
2017-01-25 18:56:48 +0800

    [WIP][Core][Channel] Refactor Channel push_channel and some relevant things.

--- core/channel/push_combined_channel.hpp
+++ core/channel/push_combined_channel.hpp
@@ -38,14 +38,6 @@ class PushCombinedChannel : public ChannelBase {
    public:
     PushCombinedChannel() = default;
 
-    // Are the following necessary?
-
-    PushCombinedChannel(const PushCombinedChannel&) = delete;
-    PushCombinedChannel& operator=(const PushCombinedChannel&) = delete;
-
-    PushCombinedChannel(PushCombinedChannel&&) = default;
-    PushCombinedChannel& operator=(PushCombinedChannel&&) = default;
-
     // The following are virtual methods
 
     void pre_send() override {
@@ -54,22 +46,28 @@ class PushCombinedChannel : public ChannelBase {
         this->shuffle_combiner_impl_->combine(&send_buffer_);
     }
 
-    void send() {
+    void send() override {
         int start = this->global_id_;
         for (int i = 0; i < send_buffer_.size(); ++i) {
             int dst = (start + i) % send_buffer_.size();
             if (send_buffer_[dst].size() == 0)
                 continue;
-            this->mailbox_->send(dst, this->channel_id_, this->progress_+1, send_buffer_[dst]);
+            this->mailbox_->send(dst, this->channel_id_, this->progress_ + 1, send_buffer_[dst]);
             send_buffer_[dst].purge();
         }
     }
 
-    void post_send() {
+    void post_send() override {
         this->inc_progress();
         this->mailbox_->send_complete(this->channel_id_, this->progress_, this->worker_info_->get_local_tids(),
                                       this->worker_info_->get_pids());
         clear_recv_buffer_();
+    }
+
+    void set_worker_info(const WorkerInfo& worker_info) override {
+        worker_info_.reset(new WorkerInfo(worker_info));
+        if (send_buffer_.size() != worker_info_->get_largest_tid() + 1)
+            send_buffer_.resize(worker_info_->get_largest_tid() + 1);
     }
 
     // The following are specific to this channel type
@@ -78,11 +76,7 @@ class PushCombinedChannel : public ChannelBase {
         shuffle_combiner_impl_.reset(combiner_base);
         shuffle_combiner_impl_->set_local_id(local_id_);
         shuffle_combiner_impl_->set_channel_id(channel_id_);
-        shuffle_combiner_impl_->set_worker_info(*(worker_info_.get())); // FIXME(fan) unsafe
-
-        // FIXME(fan) This should not be here
-        if(send_buffer_.size() != worker_info_->get_largest_tid()+1)
-            send_buffer_.resize(worker_info_->get_largest_tid()+1);
+        shuffle_combiner_impl_->set_worker_info(*(worker_info_.get()));  // FIXME(fan) unsafe
     }
 
     inline void push(const MsgT& msg, const typename DstObjT::KeyT& key) {
@@ -91,44 +85,40 @@ class PushCombinedChannel : public ChannelBase {
     }
 
     inline const MsgT& get(const DstObjT& obj) {
-        if(this->obj_list_ptr_ == nullptr) {
-            throw base::HuskyException("Object list not set and thus cannot get message by \
-                providing an object. Please use `set_obj_list` first.");
+        if (this->obj_list_ptr_ == nullptr) {
+            throw base::HuskyException(
+                "Object list not set and thus cannot get message by providing an object. "
+                "Please use `set_obj_list` first.");
         }
-        auto idx = obj - &this->obj_list_ptr_->get_data()[0];   // FIXME(fan): unsafe
-        if(not has_msgs(idx)) return MsgT();
+        auto idx = &obj - &this->obj_list_ptr_->get_data()[0];  // FIXME(fan): unsafe
+        if (!has_msgs(idx))
+            return MsgT();
         return get(idx);
     }
 
-    inline const MsgT& get(int idx) {
-        return recv_buffer_[idx];
-    }
+    inline const MsgT& get(int idx) { return recv_buffer_[idx]; }
 
     inline bool has_msgs(const DstObjT& obj) {
-        if(this->obj_start_ptr_ == nullptr) {
-            throw base::HuskyException("Object list not set and thus cannot get message by \
-                providing an object. Please use `set_obj_list` first.");
+        if (this->obj_start_ptr_ == nullptr) {
+            throw base::HuskyException(
+                "Object list not set and thus cannot get message by providing an object. "
+                "Please use `set_obj_list` first.");
         }
-        auto idx = obj - &this->obj_list_ptr_->get_data()[0];   // FIXME(fan): unsafe
+        auto idx = &obj - &this->obj_list_ptr_->get_data()[0];  // FIXME(fan): unsafe
         return has_msgs(idx);
     }
 
     inline bool has_msgs(int idx) {
-        if (idx >= recv_buffer_.size()) return false;
+        if (idx >= recv_buffer_.size())
+            return false;
         return recv_flag_[idx];
     }
 
-    void set_obj_list(ObjList<DstObjT>* obj_list_ptr) {
-        obj_list_ptr_ = obj_list_ptr;
-    }
+    void set_obj_list(ObjList<DstObjT>* obj_list_ptr) { obj_list_ptr_ = obj_list_ptr; }
 
-    std::vector<MsgT>* get_recv_buffer() {
-        return &recv_buffer_;
-    }
+    std::vector<MsgT>* get_recv_buffer() { return &recv_buffer_; }
 
-    std::vector<bool>* get_recv_flags() {
-        return &recv_flag_;
-    }
+    std::vector<bool>* get_recv_flags() { return &recv_flag_; }
 
    protected:
     void clear_recv_buffer_() { std::fill(recv_flag_.begin(), recv_flag_.end(), false); }
